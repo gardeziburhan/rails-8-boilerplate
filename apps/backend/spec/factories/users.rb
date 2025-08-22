d42deb328b83dc196b67a1a18327d0386f31de05
Burhan <burhan@techdots.dev>
2025-08-22 17:12:04 +0500

    Update user factory

--- apps/backend/spec/factories/users.rb
+++ apps/backend/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
-    password { "supersecretpassword" }
+    sequence(:email) { |n| "user#{n}@techdots.dev" }
+    password { "!Supersecretpassword101" }
     verified { true }
   end
 end
