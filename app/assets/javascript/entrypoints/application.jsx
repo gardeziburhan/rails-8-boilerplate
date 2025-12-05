e5c36159f9a5733fff080678eb08bc6253ca3425
Burhan <burhan@techdots.dev>
2025-12-05 15:57:23 +0500

    resolve CSS issue

--- app/assets/javascript/entrypoints/application.jsx
+++ app/assets/javascript/entrypoints/application.jsx
@@ -1,9 +1,10 @@
-// app/assets/javascript/entrypoints/application.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../App"; // NOTE: this path matches your App.jsx at app/assets/javascript/App.jsx
-// import "../styles/application.css"; // optional, if/when you create CSS
-
+import App from "../App";
+import "../App.css";
+import "../lib/posthog";
+import "../../javascript/App.css";
+import "../../javascript/index.css";
 document.addEventListener("DOMContentLoaded", () => {
   const rootElement = document.getElementById("root");
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def frontend_asset_tags
     safe_join(
       [
-        # stylesheet_link_tag("application", "data-turbo-track": "reload"),
+        stylesheet_link_tag("application", "data-turbo-track": "reload"),
         javascript_include_tag("application", type: "module", "data-turbo-track": "reload"),
       ],
     )
